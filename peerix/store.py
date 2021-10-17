992409416522c1c4f0685d608553d6cd13c4c0b1
Sarah <cid+git@cid-chan.moe>
2021-10-17 04:05:22 +0200

    prevent double newline.

--- peerix/store.py
+++ peerix/store.py
@@ -25,7 +25,7 @@ class NarInfo(t.NamedTuple):
             lines.append(f"Deriver: {self.deriver} ")
         for sig in self.signatures:
             lines.append(f"Sig: {sig}")
-        lines.append("\n")
+        lines.append("")
         return "\n".join(lines)
 
     @classmethod

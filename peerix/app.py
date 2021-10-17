6be67a19161152cd8c9da9508bc0ea02d158fab1
Sarah <cid+git@cid-chan.moe>
2021-10-17 04:08:15 +0200

    Remove some debugging.

--- peerix/app.py
+++ peerix/app.py
@@ -50,10 +50,6 @@ async def narinfo(req: Request) -> Response:
     if ni is None:
         return Response(content="Not found", status_code=404)
 
-
-    print(f"=== REMOTE {req.path_params['hash']} ===")
-    print(ni.dump())
-    print(f"=== REMOTE END ===")
     return Response(content=ni.dump(), status_code=200, media_type="text/x-nix-narinfo")
 
 @app.route("/local/{hash:str}.narinfo")
@@ -61,10 +57,6 @@ async def access_narinfo(req: Request) -> Response:
     ni = await l_access.narinfo(req.path_params["hash"])
     if ni is None:
         return Response(content="Not found", status_code=404)
-
-    print(f"=== LOCAL {req.path_params['hash']} ===")
-    print(ni.dump())
-    print(f"=== LOCAL END ===")
     return Response(content=ni.dump(), status_code=200, media_type="text/x-nix-narinfo")
 
 
